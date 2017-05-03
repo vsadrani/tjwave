b2e2feb8b2dbb82d9989e99476ebc1e092ce2cab
vsadrani <vidhi.sadrani@gmail.com>
2017-05-03 16:04:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // Please replace the username and password with your bluemix credentials
 
 
-exports.STTPassword = ''; // Speech to text password
-exports.STTUsername = ''; // Speech to text username
+exports.STTPassword = 'Kl6ASfJl4dmu'; // Speech to text password
+exports.STTUsername = '067aae73-253d-4cde-9bc1-93b4766e5c48'; // Speech to text username
 exports.STTCustomizationid = '' // Speech to text Customization id (if you are using a customized language model)
 
 //Credentials for Watson Text to Speech service
 
-exports.TTSPassword = ''; // Text to speech password
-exports.TTSUsername = '';
+exports.TTSPassword = '8QdWmFUbuV34'; // Text to speech password
+exports.TTSUsername = 'a3d5d6fd-6fe3-4f93-8171-3d8802b74081';
 
 // Credentials for Watson Conversation service
 exports.ConPassword = ''; // conversation service password
