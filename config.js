c5a5f6fb2a6d47b888f2668fa61d62811c19c6bc
vsadrani <vidhi.sadrani@gmail.com>
2017-05-03 15:54:49 -0400

    Create config.js
    Adding Watson credentials

---
+++ config.js
@@ -0,0 +1,22 @@
+// Please replace the username and password with your bluemix credentials
+
+
+exports.STTPassword = ''; // Speech to text password
+exports.STTUsername = ''; // Speech to text username
+exports.STTCustomizationid = '' // Speech to text Customization id (if you are using a customized language model)
+
+//Credentials for Watson Text to Speech service
+
+exports.TTSPassword = ''; // Text to speech password
+exports.TTSUsername = '';
+
+// Credentials for Watson Conversation service
+exports.ConPassword = ''; // conversation service password
+exports.ConUsername = ''; // conversation service username
+exports.ConWorkspace = ''; // Conversation workspace id
+
+// Credentials for Vision Api
+exports.VisionKey = '';
+exports.VisionVersion = '2016-05-19';
+
+exports.version = 'v1';
